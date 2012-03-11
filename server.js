8e3b7843adc78c7a891ab4f16f935e2ad5e5fdf9
nic luciano <caden@catshirt.net>
2012-03-11 04:22:39 -0400

    updating to use socket.osc

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var Connect = require('connect'),
     http_serv = Connect.createServer(),
     io = require('socket.io').listen(http_serv),
-    osc = require('omgosc'),
-    dgram = require('dgram'),
-    osc_serv = new osc.UdpReceiver(8000);
+    osc_serv = require(__dirname + '/node_modules/socket.osc');
 
 http_serv.use(Connect.static(__dirname + '/webroot'));
 http_serv.listen(8080);
-
-io.sockets.on('connection', function (socket) {
-
-    osc_serv.on('', function(msg) {
-        socket.emit('osc', msg);
-    });
-
-});
+osc_serv.listen(io, 8000);
