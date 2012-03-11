936950a82ecb801e750271ae694c7a3de8c6a507
nic luciano <caden@catshirt.net>
2012-03-11 03:18:14 -0400

    using omgosc as osc support, finishing up

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
 var Connect = require('connect'),
     http_serv = Connect.createServer(),
     io = require('socket.io').listen(http_serv),
-    // osc = require('osc-min'),
+    osc = require('omgosc'),
     dgram = require('dgram'),
-    osc_serv = dgram.createSocket('udp4');
+    osc_serv = new osc.UdpReceiver(8000);
 
 http_serv.use(Connect.static(__dirname + '/webroot'));
 http_serv.listen(8080);
-osc_serv.bind(8000);
 
 io.sockets.on('connection', function (socket) {
 
-	osc_serv.on('message', function (msg) {
-		console.log(msg);
-		/*
-	    console.log(osc.fromBuffer(msg));
-	    socket.emit('message', msg);
-        */
-	});
-    
-    setInterval(function() {
-        socket.emit('change:left', {
-            x: Math.random() * 200,
-            y: Math.random() * 200,
-            z: Math.random() * 200
-        });
-    }, 1000);
-    
+    osc_serv.on('', function(msg) {
+        socket.emit('osc', msg);
+    });
+
 });
--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,46 +1,69 @@
 (function() {
 
-    var socket = io.connect('http://localhost');
+    var socket = io.connect('http://localhost'),
+        $body = $('body'),
+        $win = $(window),
+        win_height = $win.height(),
+        win_width = $win.width();
+
+    function normalizePoint(value) {
+        value *= 100;
+        var num = Math.round(value);
+        value = num / 100;
+        return value;
+    }
 
     var HandView = Backbone.View.extend({
 
     	initialize: function(options) {
 
     		var self = this,
-                model = new Backbone.Model();
+                model = new Backbone.Model(),
+                path = '\/kinect\/' + options.path + '_(.*)?';
 
     		model.bind('change:x', _.bind(self.setX, self));
     		model.bind('change:y', _.bind(self.setY, self));
     		model.bind('change:z', _.bind(self.setZ, self));
 
-    		socket.on('change:' + options.path, function(coords) {
-    			model.set(coords);
-    		});
+            socket.on('osc', function(msg) {
+                var match = msg.path.match(path),
+                    opts = {};
+                if (match) {
+                    opts[match[1]] = msg.params[0];
+                    model.set(opts);
+                }
+            });
 
     	},
 
     	setX: function(m, x) {
+            var val = normalizePoint(x);
     		this.$el.css({
-    			left: x
+    			left: Math.round(win_width * val)
     		});
     	},
 
     	setY: function(m, y) {
+            var val = normalizePoint(y);
     		this.$el.css({
-    			top: y
+    			bottom: Math.round(win_height * val)
     		});
     	},
 
     	setZ: function(m, z) {
-    		console.log('setting z', z);
+            var val = normalizePoint(z) + 1;
+            this.$el.css({
+                zoom: val * 100 + '%'
+            });
     	},
 
     	render: function() {
     		return this.$el.css({
     			background: 'black',
-    			width: 20,
-    			height: 20,
-    			position: 'absolute'
+    			width: 60,
+    			height: 60,
+    			position: 'absolute',
+                borderRadius: 30
     		});
     	}
 
